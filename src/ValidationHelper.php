d1121a8ea3323d41b53ec9c5b231cfb32e0896dc
Byron Adams <byron.adams54@gmail.com>
2019-12-09 08:12:29 +1300

    #1 Adding Filter Arg to deleteRule, also adding tests.

--- src/ValidationHelper.php
+++ src/ValidationHelper.php
@@ -14,31 +14,34 @@ class ValidationHelper
      * @example Helpers::deleteRule($rules, 'required', array('foo', 'bar')); // 'foo' and 'bar' are removed from the required rule.
      *
      * @param Array $rules - Rules to modify
-     * @param String $target_rule - The rule to target
-     * @param string|[] $fields - fields to remove from target rule.
-     *
+     * @param $targetRule
+     * @param array $fields
+     * @param \Closure|null $filter
      * @return array $rules
      */
-    public static function deleteRule($rules, $target_rule, $fields = array())
+    public static function deleteRule($rules, $targetRule, $fields = [], \Closure $filter = null)
     {
-        $fields = is_array($fields) ? $fields : array($fields);
+        $fields = is_array($fields) ? $fields : [$fields];
+
         foreach($rules as $rk => &$rule) {
             $rule_name = $rule[1];
-            if ($target_rule == $rule_name) {
+            if ($targetRule == $rule_name && (null === $filter || true === $filter($rule))) {
                 $rule_fields  = !is_array($rule[0]) ? [$rule[0]] : $rule[0];
                 if (!empty($fields)) {
-                    foreach($rule_fields as $fk => $field) {
+                    foreach ($rule_fields as $fk => $field) {
                         if (in_array($field, $fields)) {
                             unset($rule_fields[$fk]);
                         }
                     }
-                    $rule[0] = $rule_fields;
-                } else {
+                    $rule[0] = array_values($rule_fields);
+                }
+
+                if (empty($fields) || empty($rule_fields)) {
                     unset($rules[$rk]);
                 }
             }
         }
 
-        return $rules;
+        return array_values($rules);
     }
 }
